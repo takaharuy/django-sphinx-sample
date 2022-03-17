6d13a9f3b287dde74ac3acc733a9da478a1162ef
takaharuy <takaharu-y@u-jit.info>
2022-03-17 16:01:18 +0900

    change Django database setting to PostgreSQL

--- django_sphinx_sample/django_sphinx_sample/settings.py
+++ django_sphinx_sample/django_sphinx_sample/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'django_sphinx_sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'courselist_sample',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
 }
 
