79c38e589beb57e7dde364b011f43209efa35859
takaharuy <takaharu-y@u-jit.info>
2022-03-17 10:05:44 +0900

    set up routing

--- django_sphinx_sample/django_sphinx_sample/settings.py
+++ django_sphinx_sample/django_sphinx_sample/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'courselist/static/'
+
+STATICFILES_DIRS = [
+    STATIC_URL,
+    'courselist/static/docs/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
